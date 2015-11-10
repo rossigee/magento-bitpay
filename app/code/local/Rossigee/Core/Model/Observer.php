93fb3ab83425ad0c9f0aaa76f3797ba6d21c3ee1
Ross Golder <ross@golder.org>
2015-11-10 15:11:16 +0700

    Ensure autoloader gets registered.

--- app/code/local/Rossigee/Core/Model/Observer.php
+++ app/code/local/Rossigee/Core/Model/Observer.php
@@ -4,6 +4,7 @@ class Rossigee_Core_Model_Observer {
     public function addAutoloader() {
         $file = dirname(__DIR__) . DIRECTORY_SEPARATOR . "Bitpay" . DIRECTORY_SEPARATOR . "Autoloader.php";
         require_once $file;
+        Rossigee_Core_Bitpay_Autoloader::register();
         return $this;
     }
 }
