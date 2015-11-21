f89ed6f475ab0be0484ccd677c75bcb9e536e972
Ross Golder <ross@golder.org>
2015-11-21 16:44:02 +0700

    Look for autoloader relative to docroot first.

--- app/code/local/Rossigee/Core/Model/Observer.php
+++ app/code/local/Rossigee/Core/Model/Observer.php
@@ -2,7 +2,10 @@
 
 class Rossigee_Core_Model_Observer {
     public function addAutoloader() {
-        $file = dirname(__DIR__) . DIRECTORY_SEPARATOR . "Bitpay" . DIRECTORY_SEPARATOR . "Autoloader.php";
+        $file = getenv("MAGE_DOC_ROOT") . "/app/code/local/Rossigee/Core/Bitpay/Autoloader.php";
+        if(!file_exists($file)) {
+            $file = dirname(__DIR__) . DIRECTORY_SEPARATOR . "Bitpay" . DIRECTORY_SEPARATOR . "Autoloader.php";
+        }
         require_once $file;
         Rossigee_Core_Bitpay_Autoloader::register();
         return $this;
